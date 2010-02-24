6ae29b0cfb1acb5f4260e3279b8977a972fa4ec6
Seán Hayes <sean@seanhayes.name>
2010-02-23 23:24:07 -0500

    images now get saved, implmented progress bar updating, fixed some html and css. I now need to get images with partial opacity to save properly (they're getting saved at 100%).

--- psd2html.py
+++ psd2html.py
@@ -41,6 +41,16 @@ def plugin_func(image, drawable, opaque_image_format, transparent_image_format, 
 	When testing in the console, call this function with:
 	plugin_func(gimp.image_list()[0], gimp.image_list()[0], 'png', 'png', 'png')
 	"""
+	global progress
+	#step used for progress bar. 1 for html file, 1 for css file, 3 for each layer
+	step_size = 1 / (2 + 3 * len(image.layers))
+	progress = 0
+	def add_progress(steps):
+		global progress
+		progress += steps * step_size
+		gimp.progress_update(progress)
+	gimp.progress_init('psd2html: Running')
+	add_progress(0)
 	#get name of file
 	filename = os.path.splitext(image.filename)[0]
 	directory = os.path.dirname(image.filename)
@@ -61,21 +71,23 @@ def plugin_func(image, drawable, opaque_image_format, transparent_image_format, 
 	#iterate through layers, bottom first (image.layers is ordered top first, bottom last)
 	for layer in reversed(image.layers):
 		name = layer.name
+		gimp.progress_init('psd2html: Inspecting %s' % name)
 		#replace disallowed characters with an underscore
 		element_id = disallowed_chars.sub('_', name.lower())
 		#remove leading non-letters
 		element_id = leading_nonletters.sub(r'\2_\1', element_id)
 		#remove leading and trailing underscores
 		element_id = element_id.strip('_')
+		#TODO: test for empty and duplicate ids
 		x, y = layer.offsets
 		width = layer.width
 		height = layer.height
 		#for debugging
 		print element_id
 		image_ext = 'png'
-		image_file = os.path.join(directory, media_dir, element_id+os.path.extsep+image_ext)
+		image_path = os.path.join(directory, media_dir, element_id+os.path.extsep+image_ext)
 		#the path relative from the css file
-		image_rel_path = os.path.relpath(image_file, os.path.dirname(css_file_path))
+		image_rel_path = os.path.relpath(image_path, os.path.dirname(css_file_path))
 		inner_html += "\t<div id=\"%s\"></div>\n" % element_id
 		#only absolute positioning supported, at least for now
 		css += """#%s
@@ -88,9 +100,16 @@ def plugin_func(image, drawable, opaque_image_format, transparent_image_format, 
 	height: %dpx;
 }
 
-""" % (element_id, image_rel_path, x, y, width, height)
+""" % (element_id, image_rel_path, y, x, width, height)
+		
 		#if layer is an image extract it to filename_files/ and create <div> element with a background-image set
+		
+		add_progress(1)
+		gimp.progress_init('psd2html: Saving %s' % image_path)
+		pdb.gimp_file_save(image, layer, image_path, image_path)
+		add_progress(2)
 		#to do later: if layer is text, create a text node
+	
 	#TODO: needs a clearing stylesheet
 	html = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -98,16 +117,24 @@ def plugin_func(image, drawable, opaque_image_format, transparent_image_format, 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="generator" content="psd2html GIMP Plug-in" />
+	<link href="%s" rel="stylesheet" type="text/css" />
 </head>
 <body>
 %s</body>
 </html>
-""" % inner_html
+""" % (os.path.relpath(css_file_path, os.path.dirname(html_file_path)), inner_html)
 	
+	gimp.progress_init('psd2html: Saving %s' % html_file_path)
 	os.write(html_file, html)
 	os.close(html_file)
+	add_progress(1)
+	gimp.progress_init('psd2html: Saving %s' % css_file_path)
 	os.write(css_file, css)
 	os.close(css_file)
+	add_progress(1)
+	
+	gimp.progress_init('psd2html: Finished')
+	gimp.progress_update(1)
 	#to do later: arrange nodes into a heirarchy based on size and position. Elements that fit within another element should be a child node.
 	#to do later: detect if multiple layers have the same size and position and treat them like buttons. Option to extract as css sprites.
 	#to do later: use an XML library to construct .html file. Use an XHTML 1.0 Strict doctype and validate the markup.
